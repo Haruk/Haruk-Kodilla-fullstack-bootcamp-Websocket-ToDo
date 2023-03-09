13456fb26f1c76679b8dc131603beaed9e3bcd8e
Aleksandra <haruka.lex@gmail.com>
2023-03-09 13:20:12 +0100

    Add server comunication

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
-
 const app = express();
+const socket = require('socket.io');
+
+const task = [];
+
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', ({ task, id }) => {
+    tasks.push({ task, id });
+    socket.broadcast.emit('addTask', { user, id });
+  });
+
+  socket.on('removeTask', (id) => {
+    const index = tasks.findIndex((element) => element.id == id);
+    if (index != -1) {
+      tasks.splice(index, 1);
+    }
+    socket.broadcast.emit('removeTask', id);
+  });
+
 });
